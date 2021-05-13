abad6c82b07fbbb7769e5dadd13ddb2f1c2c222d
sverissimo <sandroverissimo@live.com>
2021-05-13 00:40:57 -0300

    criação de loop para iterar sobre cada arquivo recebido via socket, gerando nomes de pastas e subpastas, as criando e salvando o arquivo para cada arquivo recebido. Clean do código no serviço FolderService.ts

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -6,7 +6,26 @@ class FileService {
 
     async saveFile({ files, fields }) {
 
-        const
+        let i = 0
+        for (let f of files) {
+            const
+                data = Buffer.from(f, 'base64')
+                , { filename, metadata } = fields[i] && fields[i]
+                , { fieldName, empresaId } = metadata
+                , file = new FileEntity(empresaId, fieldName)
+                , folderService = new FolderService()
+                , folder = await folderService.getFolderName(file)
+                , path = folder
+            i++
+
+            //console.log("🚀 ~ file: tst.js ~ line 19 ~ files", { fieldName, empresaId }, fields)
+            if (!fs.existsSync(path))
+                fs.mkdirSync(path, { recursive: true })
+
+            fs.writeFileSync(path + filename, data)
+        }
+
+        /* const
             data = Buffer.from(files, 'base64')
             , { filename, metadata } = fields[0] && fields[0]
             , { fieldName, empresaId } = metadata
@@ -20,7 +39,7 @@ class FileService {
         if (!fs.existsSync(path))
             fs.mkdirSync(path, { recursive: true })
 
-        fs.writeFileSync(path + filename, data)
+        fs.writeFileSync(path + filename, data) */
     }
 }
 
--- src/services/FolderService.ts
+++ src/services/FolderService.ts
@@ -2,19 +2,14 @@ import { MainController } from "../controllers/MainController";
 import { FileEntity } from "../entities/FileEntity";
 
 
-interface IFolder {
-    codigoEmpresa: number
-    subfolderName: string
-}
-
-
 export class FolderService {
 
     async getFolderName(file: FileEntity) {
         const
             { codigoEmpresa, subfolderName } = file
             , baseFolder = 'd:\\ARQUIVOS_CADTI'
-            , razaoSocial = await this.getEmpresaName(codigoEmpresa)
+            , razaoSocialRaw = await this.getEmpresaName(codigoEmpresa)
+            , razaoSocial = razaoSocialRaw.replace('\/', ' ')
             , rootFolder = `${codigoEmpresa} - ${razaoSocial}`
             , folderName = `${baseFolder}\\${rootFolder}\\${subfolderName}\\`
 
@@ -22,7 +17,6 @@ export class FolderService {
     }
 
     async getEmpresaName(codigoEmpresa: number): Promise<any> {
-        console.log("🚀 ~ file: EmpresaService.ts ~ line 9 ~ EmpresaService ~ getEmpresaName ~ codigoEmpresa", codigoEmpresa)
         const
             controller = new MainController()
             , { razao_social } = await controller.getEmpresa(codigoEmpresa)
@@ -30,8 +24,8 @@ export class FolderService {
         return razao_social
     }
 
-    getVeiculoPlate?(veiculoId: number) {
+    /* getVeiculoPlate?(veiculoId: number) {
 
-    }
+    } */
 
 }
